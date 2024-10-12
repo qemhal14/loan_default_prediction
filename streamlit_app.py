e33bb8270688c8331183e53ccfd3438c5c7ba6c5
qemhal14 <124538307+qemhal14@users.noreply.github.com>
2024-10-12 10:35:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pickle
 from xgboost.sklearn import XGBClassifier
 
 with open('loan_default_predictor.pkl', 'rb') as model_file:
